9566c510147363f9c2506708df6a78bd80e77b88
jeff-cf <jianfuwave@gmail.com>
2024-10-19 01:13:46 +0800

    cmake設置輸出路徑

---
+++ 02_07_cmake_out/student/student.h
@@ -0,0 +1,34 @@
+#include<string>
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#ifndef _WIN32
+    //如果不是windows，則導出庫，不需要添加__declspec()宏
+    #define STUDENT_API
+#else
+    #ifdef student_STATIC
+        #define STUDENT_API
+    #else
+        
+        #ifdef student_EXPORTS
+            #define STUDENT_API __declspec(dllexport)
+        #else
+            #define STUDENT_API __declspec(dllimport)
+        #endif
+
+    #endif
+ 
+
+#endif
+
+class STUDENT_API Student
+{
+private:
+    std::string name;
+    int age;
+public:
+    Student(char *name, int age);
+    ~Student();
+};
+
+#endif
---
+++ 02_07_cmake_out/test_student/test_student.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "student.h"
+
+int main(int argv, char* argc[]){
+
+    Student s1("Jeff", 18);
+    return 0;
+}
